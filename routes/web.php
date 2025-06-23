1b88d4caf45ed2958145f277212a18c99ecc3464
Ankangh001 <ankanghosh010@gmail.com>
2025-06-23 21:31:38 +0000

    prod

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+
+use Illuminate\Support\Facades\Response;
+
+Route::get('/direct-image/{filename}', function ($filename) {
+    $path = storage_path('app/public/uploads/profile/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404, 'File not found.');
+    }
+
+    return Response::file($path);
+});
