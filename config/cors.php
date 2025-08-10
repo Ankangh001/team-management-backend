c40a4546a4b06f28569b72e0f63c595c653d1ba2
Ankangh001 <ankanghosh010@gmail.com>
2025-08-10 14:56:32 +0530

    update fixes

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_origins' => [
         'http://localhost:8080',
         'https://tea-management-frontend.vercel.app',
+        'http://app.esamudaay.com', 
+        'https://app.esamudaay.com'
     ],
 
     'allowed_methods' => ['*'],
