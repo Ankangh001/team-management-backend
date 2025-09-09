fdfdb8f334cab38f4411750dfa4301c3efa10c86
Ankangh001 <your.email@example.com>
2025-09-10 00:52:04 +0530

    Resolve merge: keep remote config/cors.php

--- config/cors.php
+++ config/cors.php
@@ -4,10 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     'allowed_origins' => [
-        'http://localhost:8080',
-        'https://tea-management-frontend.vercel.app',
-        'http://app.esamudaay.com', 
-        'https://app.esamudaay.com'
+       '*',
     ],
 
     'allowed_methods' => ['*'],
